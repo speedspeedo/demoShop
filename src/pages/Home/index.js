dbf0ad65f2fb331e0aea02e81dea992838d77f5b
speedo1004 <speedodeveloper1004@gmail.com>
2023-01-28 23:02:46 +0900

    button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import './index.css';
 
 const Home = () => {
 
@@ -16,14 +17,14 @@ const Home = () => {
         <div>
             <button 
                 type="button" 
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-2xl px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-8xl px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={joinOnline}
             >
                 Online
             </button>
             <button 
                 type="button" 
-                class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-2xl px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-8xl px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 onClick={joinOffline}
             >
                 Offline
