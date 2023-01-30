d00dd67b9fbd9573aab5f53b33c800b0cf5a9099
speedo1004 <speedodeveloper1004@gmail.com>
2023-01-30 12:17:24 +0900

    Add Navbar

--- src/pages/Offline/index.js
+++ src/pages/Offline/index.js
@@ -57,13 +57,13 @@ const OfflineShop = () => {
     <div class="mb-6 w-1/2 mx-auto">
       <label
         for="countries"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        class="block mb-2 text-6xl font-medium text-gray-900 dark:text-white"
       >
         Select an option
       </label>
       <select
         id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         onChange={handleChange}
       >
         <option selected>--Choose whatever you want--</option>
@@ -73,7 +73,7 @@ const OfflineShop = () => {
 
       <button
           type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-8xl px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-6xl px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={showResult}
         >
           Search
